616e10d54082ced7a847f8c3d9d1088a3f4c53e4
Christopher Giordano <chrisgcpg@gmail.com>
2024-07-17 11:05:46 -0400

    Added data and seeded users and thoughts.

--- utils/data.js
+++ utils/data.js
@@ -1,22 +1,23 @@
 const users = [
     {
-        "username": "Christopher",
-        "email": "chrisgcpg@gmail.com"
+        username: "Christopher",
+        email: "chrisgcpg@gmail.com"
     },
     {
-        "username": "Nicole",
-        "email": "nicoledimenna@gmail.com"
+        username: "Nicole",
+        email: "nicoledimenna@gmail.com"
     
     }
 ];
 const thoughts = [
     {
-        "thoughtText": "This is a thought example 1",
+        thoughtText: "This is a thought example 1",
     },
     {
-        "thoughtText": "This is a thought example 2",
+        thoughtText: "This is a thought example 2",
     },
     {
-        "thoughtText": "This is a thought example 3",
+        thoughtText: "This is a thought example 3",
     }
 ]
+module.exports = { users, thoughts };
